c96b8e4e652fe761ab74c2e5e618e8246a683571
peluche <me@peluche.im>
2016-10-22 16:09:57 +0200

    Draw a single image into the canvas 🍭

---
+++ jiongtu/static/main.js
@@ -0,0 +1,35 @@
+var canvas = document.querySelector("#jiong");
+var ctx = canvas.getContext('2d');
+
+function create_image(pixels) {
+    // pixels are (x, y, r, g, b)
+    var w = 100, h = 100;
+    var imagedata = ctx.createImageData(w, h);
+    pixels.forEach(function(pixel) {
+        var x = pixel[0], y = pixel[1];
+        imagedata.data[((w * y) + x) * 4] = pixel[2];
+        imagedata.data[((w * y) + x) * 4 + 1] = pixel[3];
+        imagedata.data[((w * y) + x) * 4 + 2] = pixel[4];
+        imagedata.data[((w * y) + x) * 4 + 3] = 255;
+    });
+    return imagedata;
+}
+
+function draw(pixels) {
+    ctx.putImageData(create_image(pixels), 0, 0);
+}
+
+function getData() {
+    var request = new XMLHttpRequest();
+    request.open('GET', '/i', true);
+    request.onload = function() {
+        var response = JSON.parse(request.responseText);
+        var images = response['images'];
+        if (images.length > 0) {
+            draw(images[0]);
+        }
+    }
+    request.send();
+}
+
+getData();
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'jiongtu',
+    packages = ['jiongtu'],
+    version = '0.1',
+    description = '囧',
+    author = 'peluche',
+    author_email = '',
+    url = 'https://github.com/peluche/jiongtu',
+    download_url = 'https://github.com/peluche/jiongtu/tarball/0.1',
+    install_requires = [
+        'flask',
+    ],
+    keywords = [],
+    classifiers = [])
