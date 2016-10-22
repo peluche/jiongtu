82441c1b3bc2da5035a7e1c7c744da25f324c8d6
peluche <me@peluche.im>
2016-10-22 20:03:14 +0200

    update setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'jiongtu',
     packages = ['jiongtu'],
-    version = '0.1.0',
+    version = '0.1',
     description = '囧',
     author = 'peluche',
     author_email = '',
